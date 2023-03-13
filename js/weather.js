5deeae348f435f402cb15aa6b47506acf120f4d7
fuchen <zhaolexe@gmail.com>
2023-03-13 17:01:01 +0800

    Site updated: 2023-03-13 17:00:57

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "12043",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "zh",
+    "borderRadius": "5",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "city": "CN101110113",
+    "left": "120",
+    "top": "7",
+    "key": "b9ddba9476874b1c905458458e7691f6"
+  }
+}
