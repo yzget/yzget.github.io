04e35b24a41ad7358acdbdb566cff68099eef2eb
yzget <zhaolexe@gmail.com>
2026-07-01 12:59:05 +0800

    Automated spider update: 2026-07-01 12:59:02

---
+++ wp-login.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+	<html lang="zh-Hans" scheme="light">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>登录 &lsaquo; 蝴蝶飞 &#8212; WordPress</title>
+	<meta name='robots' content='max-image-preview:large, noindex, noarchive' />
+<link rel='stylesheet' href='http://localhost:10004/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5Bchunk_0%5D=dashicons,buttons,forms,l10n,wp-base-styles,login&amp;ver=7.0' media='all' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="icon" href="http://localhost:10004/wp-content/uploads/2026/06/cropped-hudiefei-32x32.png" sizes="32x32" />
+<link rel="icon" href="http://localhost:10004/wp-content/uploads/2026/06/cropped-hudiefei-192x192.png" sizes="192x192" />
+<link rel="apple-touch-icon" href="http://localhost:10004/wp-content/uploads/2026/06/cropped-hudiefei-180x180.png" />
+<meta name="msapplication-TileImage" content="http://localhost:10004/wp-content/uploads/2026/06/cropped-hudiefei-270x270.png" />
+	</head>
+	<body class="login no-js login-action-login wp-core-ui admin-color-modern locale-zh-cn">
+	<script>
+document.body.className = document.body.className.replace('no-js','js');
+</script>
+
+				<h1 class="screen-reader-text">登录</h1>
+			<div id="login">
+		<h1 role="presentation" class="wp-login-logo"><a href="https://cn.wordpress.org/">基于 WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="http://localhost:10004/wp-login.php" method="post">
+			<p>
+				<label for="user_login">用户名或邮箱地址</label>
+				<input type="text" name="log" id="user_login" class="input ltr" value="" size="20" autocapitalize="off" autocomplete="username" required="required" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">密码</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input ltr" value="" size="20" autocomplete="current-password" spellcheck="false" required="required" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="显示密码">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">记住我</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="登录" />
+									<input type="hidden" name="redirect_to" value="http://localhost:10004/2026/06/29/%e6%9c%80%e6%96%b0guisingbox%e8%b7%af%e7%94%b1%e8%a7%84%e5%88%99%ef%bc%8c%e8%a7%84%e5%88%99-dns%e4%bf%ae%e6%94%b9/" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+				<a class="wp-login-lost-password" href="http://localhost:10004/wp-login.php?action=lostpassword">忘记密码？</a>			</p>
+			<script>
+function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }
+</script>
+		<p id="backtoblog">
+			<a href="http://localhost:10004/">&larr; 返回到 蝴蝶飞</a>		</p>
+			</div>
+				<div class="language-switcher">
+				<form id="language-switcher" method="get">
+
+					<label for="language-switcher-locales">
+						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
+						<span class="screen-reader-text">
+							语言						</span>
+					</label>
+
+					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
+<option value="zh_CN" lang="zh" selected='selected' data-installed="1">简体中文</option>
+<option value="zh_TW" lang="zh" data-installed="1">繁體中文</option></select>
+					
+											<input type="hidden" name="redirect_to" value="http://localhost:10004/2026/06/29/%e6%9c%80%e6%96%b0guisingbox%e8%b7%af%e7%94%b1%e8%a7%84%e5%88%99%ef%bc%8c%e8%a7%84%e5%88%99-dns%e4%bf%ae%e6%94%b9/" />
+					
+					
+						<input type="submit" class="button" value="更改">
+
+					</form>
+				</div>
+			
+	
+<script>
+var _zxcvbnSettings = {"src":"http://localhost:10004/wp-includes/js/zxcvbn.min.js"};
+//# sourceURL=js-inline-concat-clipboard%2Cjquery-core%2Cjquery-migrate%2Czxcvbn-async%2Cwp-hooks
+</script>
+<script src='http://localhost:10004/wp-admin/load-scripts.php?c=1&amp;load%5Bchunk_0%5D=clipboard,jquery-core,jquery-migrate,zxcvbn-async,wp-hooks&amp;ver=7.0'></script>
+<script id="wp-i18n-js" src="http://localhost:10004/wp-includes/js/dist/i18n.min.js?ver=781d11515ad3d91786ec"></script>
+<script id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+//# sourceURL=wp-i18n-js-after
+</script>
+<script id="password-strength-meter-js-extra">
+var pwsL10n = {"unknown":"\u5bc6\u7801\u5f3a\u5ea6\u672a\u77e5","short":"\u975e\u5e38\u5f31","bad":"\u5f31","good":"\u4e2d\u7b49","strong":"\u5f3a","mismatch":"\u4e0d\u5339\u914d"};
+//# sourceURL=password-strength-meter-js-extra
+</script>
+<script id="password-strength-meter-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2026-05-21 05:54:13+0000","generator":"GlotPress\/4.0.3","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"zh_CN"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["\u81ea %2$s \u7248\u5f00\u59cb\uff0c%1$s \u5df2\u7ecf\u6dd8\u6c70\uff0c\u8bf7\u6539\u7528 %3$s\u3002\u8bf7\u8003\u8651\u64b0\u5199\u66f4\u5177\u517c\u5bb9\u6027\u7684\u4ee3\u7801\u3002"]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
+//# sourceURL=password-strength-meter-js-translations
+</script>
+<script id="password-strength-meter-js" src="http://localhost:10004/wp-admin/js/password-strength-meter.min.js?ver=7.0"></script>
+<script id="underscore-js" src="http://localhost:10004/wp-includes/js/underscore.min.js?ver=1.13.8"></script>
+<script id="wp-util-js-extra">
+var _wpUtilSettings = {"ajax":{"url":"/wp-admin/admin-ajax.php"}};
+//# sourceURL=wp-util-js-extra
+</script>
+<script id="wp-util-js" src="http://localhost:10004/wp-includes/js/wp-util.min.js?ver=7.0"></script>
+<script id="wp-dom-ready-js" src="http://localhost:10004/wp-includes/js/dist/dom-ready.min.js?ver=a06281ae5cf5500e9317"></script>
+<script id="wp-a11y-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2026-05-21 05:44:01+0000","generator":"GlotPress\/4.0.3","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"zh_CN"},"Notifications":["\u901a\u77e5"]}},"comment":{"reference":"wp-includes\/js\/dist\/a11y.js"}} );
+//# sourceURL=wp-a11y-js-translations
+</script>
+<script id="wp-a11y-js" src="http://localhost:10004/wp-includes/js/dist/a11y.min.js?ver=af934e5259bc51b8718e"></script>
+<script id="user-profile-js-extra">
+var userProfileL10n = {"user_id":"0","nonce":"d61a4638f3"};
+//# sourceURL=user-profile-js-extra
+</script>
+<script id="user-profile-js-translations">
+( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2026-05-21 05:54:13+0000","generator":"GlotPress\/4.0.3","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"zh_CN"},"Caps lock is on.":["\u5927\u5199\u9501\u5b9a\u5df2\u6253\u5f00\u3002"],"Application password has been copied to your clipboard.":["\u5e94\u7528\u7a0b\u5e8f\u5bc6\u7801\u5df2\u590d\u5236\u5230\u526a\u8d34\u677f\u3002"],"Your new password has not been saved.":["\u60a8\u7684\u65b0\u5bc6\u7801\u672a\u88ab\u4fdd\u5b58\u3002"],"Confirm use of weak password":["\u786e\u8ba4\u4f7f\u7528\u5f31\u5bc6\u7801"],"Hide password":["\u9690\u85cf\u5bc6\u7801"],"Show password":["\u663e\u793a\u5bc6\u7801"],"Hide":["\u9690\u85cf"],"Show":["\u663e\u793a"],"The changes you made will be lost if you navigate away from this page.":["\u79bb\u5f00\u8fd9\u4e2a\u9875\u9762\uff0c\u60a8\u6240\u505a\u7684\u66f4\u6539\u5c06\u4e22\u5931\u3002"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
+//# sourceURL=user-profile-js-translations
+</script>
+<script id="user-profile-js" src="http://localhost:10004/wp-admin/js/user-profile.min.js?ver=7.0"></script>
+		<script>
+			! function() {
+				var e = -1 < navigator.userAgent.toLowerCase().indexOf("webkit"),
+					t = -1 < navigator.userAgent.toLowerCase().indexOf("opera"),
+					n = -1 < navigator.userAgent.toLowerCase().indexOf("msie");
+				(e || t || n) && document.getElementById && window.addEventListener && window.addEventListener("hashchange", function() {
+					var e, t = location.hash.substring(1);
+					/^[A-z0-9_-]+$/.test(t) && (e = document.getElementById(t)) && (/^(?:a|select|input|button|textarea)$/i.test(e.tagName) || (e.tabIndex = -1), e.focus())
+				}, !1)
+			}();
+		</script>
+			</body>
+	</html>
+	
